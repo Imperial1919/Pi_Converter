03391f35312614b5cd7bb30cc1dfa5bc8934f8e2
Bryce <ImperialEagle1998@gmail.com>
2020-10-08 20:23:03 -0500

    A functional, simple pi converter.

---
+++ Are_of_Circle_Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Are_of_Circle_Method
+{
+    class Program
+    {
+
+        static double circlearea(double input)
+        {
+           return input * 3.14159264;
+           
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(circlearea(4));
+
+        }
+    }
+}
